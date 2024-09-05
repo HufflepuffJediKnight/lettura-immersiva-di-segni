54c5cd9e3d2c09ea29be6a9e838c717569cb7afa
Enrico Tavagnutti <70480634+HufflepuffJediKnight@users.noreply.github.com>
2024-09-05 15:01:30 +0200

    fixing auto-close function (have to add main video playback resume and reactivate pause button)

--- Assets/Scripts/Video_Controller.cs
+++ Assets/Scripts/Video_Controller.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 using System;
+using static UI_Controller;
 
 public class Video_Controller : MonoBehaviour
 {
     public VideoPlayer[] videoPlayers;
     VideoPlayer currentPlayer;
     public GameObject[] Hotspots;
+    public UI_Controller controller;
+    public GameObject pauseButton;
 
     void PrepareVideos()
     {
@@ -34,26 +37,34 @@ public class Video_Controller : MonoBehaviour
 
     private void Update()
     {
-        currentPlayer.loopPointReached += AutoCloseHotspots;
+        CheckVideoStatus();
+        
     }
 
+    //controlla se uno dei due videoplayer degli hotspot sta riproducendo un video
+    //se sì lo assegna alla variabile currentPlayer e alla fine della riproduzione
+    //attiverà la funzione AutoCloseHotspots
+    public void CheckVideoStatus()
+    {
+        foreach (var i in videoPlayers)
+        {
+            if (i.isPlaying)
+            {
+                currentPlayer = i;
+                currentPlayer.loopPointReached += AutoCloseHotspots;
+            }
+        }
+    }
+
+    // disattiva entrambi gli hotspots (anche se uno dei due sarà già disattivo)
+    // che prende da un array di hotspot
     private void AutoCloseHotspots(VideoPlayer source)
     {
         for (int i = 0; i < Hotspots.Length; i++)
         {
             Hotspots[i].SetActive(false);
         }
-    }
-
-    public void CheckVideoStatus()
-    {
-        for (int i = 0; i < videoPlayers.Length; i++)
-        {
-            if (videoPlayers[i].isPlaying)
-            {
-                videoPlayers[i] = currentPlayer;
-            }
-        }
+        controller.Play();
     }
 
 }
